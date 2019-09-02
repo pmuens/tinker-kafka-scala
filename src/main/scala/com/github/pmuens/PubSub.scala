90016907848d17ec9b43b3b3504c515c6c627bd4
Philipp Muens <raute1337@gmx.de>
2019-09-02 10:26:03 +0200

    Add comments

--- src/main/scala/com/github/pmuens/PubSub.scala
+++ src/main/scala/com/github/pmuens/PubSub.scala
@@ -3,6 +3,7 @@ package com.github.pmuens
 import com.github.pmuens.Main.writeToKafka
 import com.github.pmuens.Main.consumeFromKafka
 
+// 2. Run the producer (watch the consumers log output)
 object Produce extends App {
   override def main(args: Array[String]): Unit = {
     val topic = "kafka-test"
@@ -11,6 +12,7 @@ object Produce extends App {
   }
 }
 
+// 1. Run the consumer
 object Consume extends App{
   override def main(args: Array[String]): Unit = {
     val topic = "kafka-test"
